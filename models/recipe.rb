70ece92c0ca5896f1a74a182866eaa85c3f37f52
Aditi Chidambara <aditi.chidu04@gmail.com>
2026-09-11 10:31:03 -0500

    Add built-in recipes, navigation, and search

---
+++ models/recipe.rb
@@ -0,0 +1,11 @@
+class Recipe
+  attr_accessor :name, :description, :cook_time, :ingredients, :steps
+
+  def initialize(name, description, cook_time, ingredients, steps)
+    @name = name
+    @description = description
+    @cook_time = cook_time
+    @ingredients = ingredients
+    @steps = steps
+  end
+end
